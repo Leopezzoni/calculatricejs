88ad7c36ee6d8f1d4d5350ece2dc6098915c4907
Tonio <contact@wispelaere.fr>
2021-07-20 07:52:43 +0200

    add remove function with a special counter and add class

--- js/calculatrice.js
+++ js/calculatrice.js
@@ -1,42 +1,57 @@
-function remove (event) {
-  console.log("toto", event);
-}
+
 // 1. Fonction addition
+// Initialisation d'un compteur
+let counter = 1;
 function calculer(operateur) {
-  console.log("oper : ", operateur);
-  // deux variables input et une variable résultat
-  let input1 = Number(document.getElementById("input1").value),
-    input2 = Number(document.getElementById("input2").value),
-    resultatHTml = document.getElementById("result"),
-    resultat;
-  let li = document.createElement("li");
+
+  // Initialisation des variables du dom
+  let input1 = Number(document.getElementById("input1").value), // Valeur de l'input 1
+    input2 = Number(document.getElementById("input2").value), // Valeur de l'input 2
+    resultatHTml = document.getElementById("result"), // Element au quel il faudra ajouter les resultats de calculs
+    customClass, // Personalisation de la class à appliquer
+    resultat, // Variable qui contiendra le résultat du calcul
+    monId= `li-${counter}`, // initialisation de l'id à utiliser pour le ciblage
+    li = document.createElement("li"); // Création d'un élèment que nous réinserrons
+
   //switch
   // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Statements/switch
+
   switch (operateur) {
     case "+":
       resultat = `Calcul de ${input1} + ${input2} = ${input1 + input2}; `;
-      li.id = "addition";
-      console.log("+");
+      customClass = 'additionner';
       break;
     case "-":
       resultat = `Calcul de ${input1} - ${input2} = ${input1 - input2}; `;
-      console.log("-");
+      customClass = 'soustraire';
       break;
     case "*":
       resultat = `Calcul de ${input1} * ${input2} = ${input1 * input2}; `;
-      console.log("*");
+      customClass = 'multiplier';
       break;
     case "/":
       resultat = `Calcul de ${input1} / ${input2} = ${input1 / input2}; `;
-      console.log("/");
+      customClass = 'diviser';
       break;
 
     default:
       console.log("error");
   }
+  
+  li.id= monId
+  li.classList.add(customClass);
 
   // ajouter les attribut à li pour ajouter une class selon l'opérateur
-
+  resultat += `<bouton onclick=\"removeElt(event)\" data_target="${monId}">X</button>`
   li.innerHTML = resultat;
   resultatHTml.appendChild(li);
+  // Incrémentation du compteur
+  counter++;
 }
+// Fonction de suppression d'élement
+function removeElt (event) {
+  let selectedId = event.target.getAttribute("data_target")
+  let currentElt = document.getElementById(selectedId)
+  currentElt.remove()
+}
+
